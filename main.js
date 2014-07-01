98821086c30c65aa0a229f82d09df4027320d41b
Jacob <jakeboone@aol.com>
2014-06-30 22:12:29 -0600

    main clock components added

--- main.js
+++ main.js
@@ -19,15 +19,18 @@ var autoShell = $('<div class="auto-shell">AUTO</div>');
 var amShell = $('<div class="am-shell">AM</div>'); 
 var pmShell = $('<div class="pm-shell">PM</div>'); 
 var clockscreenShell = $('<div class="clockscreen-shell"></div>'); 
-var indicatorShell = $('<img class="indicator-shell src="http://rpmbuild.joshr.com/more-icons/red_dot.gif">'); 
-	
+var indicatorShell = $('<img id="indicator-shell" src="http://rpmbuild.joshr.com/more-icons/red_dot.gif">'); 
+var timetextShell = $('<div class="timetext-shell">12:00</div>');
+var amchannelShell = $('<div class="amchannel-shell">AM     53  60  70  90 110 140 170      KHz</div>'); 
+var fmchannelShell = $('<div class="fmchannel-shell">FM     88  92  96  102 106 108     MHz</div>'); 
+
 	// outer shell into the main div container
-	$('.container').append(outerShell)
-	console.log('works')
+	$('.container').append(outerShell);
+	console.log('works');
 	// puts inner shell into outershell
 	outerShell.append(innerShell);
-	innerShell.append(autoShell,amShell,pmShell,clockscreenShell)
-	clockscreenShell.append(indicatorShell)
+	innerShell.append(autoShell,amShell,pmShell,clockscreenShell,amchannelShell,fmchannelShell);
+	clockscreenShell.append(indicatorShell, timetextShell);
 
 
 
@@ -70,7 +73,7 @@ var indicatorShell = $('<img class="indicator-shell src="http://rpmbuild.joshr.c
 			background: "black",
 			color: "white",
 			position: "absolute",
-			top: 70,
+			top: 73,
 			left: 28,
 			fontSize: 10
 		})
@@ -81,7 +84,7 @@ var indicatorShell = $('<img class="indicator-shell src="http://rpmbuild.joshr.c
 			background: "black",
 			color: "white",
 			position: "absolute",
-			top: 50,
+			top: 53,
 			left: 28,
 			fontSize: 10
 		})
@@ -96,14 +99,45 @@ var indicatorShell = $('<img class="indicator-shell src="http://rpmbuild.joshr.c
 		})
 
 		$(indicatorShell).css({
-			width: 20,
-			height: 50,
-			// background: "black",
-			// color: "red",
+			width: 12,
+			height: 12,
 			position: "absolute",
-			top: 0,
-			left: 5,
-			// fontSize: 50
+			top: 4,
+			left: 4,
+	
+		})
+		$(timetextShell).css({
+			width: 80,
+			height: 20,
+			position: "absolute",
+			color: "red",
+			top: -5,
+			left: 50,
+			fontSize: 75,
+			// fontStyle: "digital"
+			fontFamily: 'BebasNeueRegular',
+		})
+
+		$(amchannelShell).css({
+			width: 300,
+			height: 10,
+			background: "black",
+			color: "white",
+			position: "absolute",
+			top: 130,
+			left: 55,
+			fontSize: 10
+		})
+
+		$(fmchannelShell).css({
+			width: 200,
+			height: 10,
+			background: "black",
+			color: "white",
+			position: "absolute",
+			top: 145,
+			left: 55,
+			fontSize: 10
 		})
 });
 	
